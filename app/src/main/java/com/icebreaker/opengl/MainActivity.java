413027c67fd2801b1e3e232b8c81bf7e81a08857
ZoneLai <185838191@qq.com>
2018-03-22 16:08:17 +0800

    frist commit.

---
+++ app/src/main/java/com/icebreaker/opengl/MainActivity.java
@@ -0,0 +1,42 @@
+package com.icebreaker.opengl;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+public class MainActivity extends Activity {
+    private EGLRenderer mGLRenderer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        SurfaceView surfaceView = (SurfaceView) findViewById(R.id.sv_main_demo);
+        mGLRenderer = new EGLRenderer();
+        mGLRenderer.start();
+        surfaceView.getHolder().addCallback(new SurfaceHolder.Callback() {
+            @Override
+            public void surfaceCreated(SurfaceHolder surfaceHolder) {
+
+            }
+
+            @Override
+            public void surfaceChanged(SurfaceHolder surfaceHolder, int format, int width, int height) {
+                mGLRenderer.onDrawFrame(surfaceHolder.getSurface(), width, height);
+            }
+
+            @Override
+            public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
+
+            }
+        });
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mGLRenderer.onRelease();
+    }
+}
